565eeabcb90b714bdde6a50292b363c7c090d165
KVAcodes <kadiriavictor@gmail.com>
2023-03-23 12:51:41 +0100

    added new tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -26,3 +26,15 @@ class TestUser(unittest.TestCase):
         self.assertTrue(hasattr(usr1, "password"))
         self.assertTrue(hasattr(usr1, "first_name"))
         self.assertTrue(hasattr(usr1, "last_name"))
+
+    def test_email_is_public_str(self):
+        self.assertEqual(str, type(User.email))
+
+    def test_password_is_public_str(self):
+        self.assertEqual(str, type(User.password))
+
+    def test_first_name_is_public_str(self):
+        self.assertEqual(str, type(User.first_name))
+
+    def test_last_name_is_public_str(self):
+        self.assertEqual(str, type(User.last_name))
